0f6f4b5db92afb8952ec66ad158637c230069dcf
hpimpalgaonkar <74096609+hpimpalgaonkar@users.noreply.github.com>
2020-12-20 15:03:01 +0530

    Add files via upload

--- heston_rough_v2.py
+++ heston_rough_v2.py
@@ -22,10 +22,11 @@ class base_heston():
     mu = drift
     K = strike price
     alpha = 1 + H - 0.5 , Hurst Coefficient
+    nu = Multiplier in the second integral
     Lambda = Parameter from the Rough Heston Model
     """
     
-    def __init__(self, mu=0.1, rho=0, sigma=0.2, theta=-0.1, kappa=0.1, K = 100, alpha=0.6,lamb = 2):
+    def __init__(self, mu=0.1, rho=0, sigma=0.2, theta=-0.1, kappa=0.1, K = 100, alpha=0.6,lamb = 0.1, nu = 0.1):
         self.mu = mu
         self.K = K
         if (np.abs(rho)>1):
@@ -39,6 +40,7 @@ class base_heston():
             self.kappa = kappa
             self.alpha = alpha
             self.lamb = lamb
+            self.nu = nu
     
     def integrate(self, S0, v0, N, T=1):
         
@@ -77,12 +79,18 @@ class base_heston():
                 x=np.arange(0,(t)*dt,(t)*dt/100)    # Create a dummy variable for integration of time, take time upto t
                 vinterp = np.interp(x,range(0,t)*dt,v[:t]) # interpolate the volatility upto time t on x
                 vsqinterp = np.sqrt(vinterp)    # Square root of the interpolated volatility
-                v[t+1] = np.maximum(v0 + 1/gamma(self.alpha)*simps(((t+1)*dt-x)**(self.alpha-1)*self.lamb*(self.theta-vinterp),x) + 1/gamma(self.alpha)*simps(((t+1)*dt-x)**(self.alpha-1)*self.lamb*self.sigma * vsqinterp * dt_sq * W_v[t],x),0)
+                v[t+1] = np.maximum(v0 + 1/gamma(self.alpha)*simps(((t+1)*dt-x)**(self.alpha-1)*self.lamb*(self.theta-vinterp),x) + 1/gamma(self.alpha)*simps(((t+1)*dt-x)**(self.alpha-1)*self.lamb*self.nu * vsqinterp * dt_sq * W_v[t],x),0)
             else:
                 v[t+1] = v0
             #Update the underlying based on volatility at previous time step
             X[t+1] = X[t] + (self.mu)*X[t]*dt + v_sq * dt_sq *X[t]* W_S[t]         #from paper, Euler
             #X[t+1] = X[t] * np.exp((self.mu - 0.5*v[t])*dt + v_sq * dt_sq * W_S[t] ) #same results as paper
+        #plt.plot(v)
+        
+        #plt.xlabel('Time*0.001 (yr)')
+        #plt.ylabel('Volatility')
+        #plt.show()
+        #input()
         return X, v
 
     #Payoff for call option - at the end of the time period
@@ -101,18 +109,18 @@ N = 1000                                                      # time points
 T = 1                                                        # units in time, here it is in years as the riskless rate is defined as per year
 T_vec, dt = np.linspace(0,T,N, retstep=True )                 # time vector and time step
 S0 = 100                                                      # initial stock price
-K = 100                                                       # Strike price
-v0 = 0.010201                                                 # initial volatility
-r = 0.0319                                                    # Riskless Rate of return 
-mu = r; rho = -0.7; kappa = 6.21; theta = 0.019; sigma = 0.61    #values for different 
-assert(2*kappa * theta <= sigma**2)                            # Condition from the paper is asserted, a method to check if the parameters are correct
-num_sims = 1000                                            #Monte Carlo simulation paths
+K = 80                                                       # Strike price
+v0 = 0.0392                                                # initial volatility
+r = 0.3                                                    # Riskless Rate of return 
+mu = 0; rho = -0.681; kappa = 6.21; theta = 0.3156; sigma = 0.6; nu = 0.331    #values for different 
+#assert(2*kappa * theta <= sigma**2)                            # Condition from the paper is asserted, a method to check if the parameters are correct
+num_sims = 100                                            #Monte Carlo simulation paths
 call_payoff = 0.0                                             #Payoff for call option
 put_payoff = 0.0                                              #Payoff for put option
 
 np.random.seed(seed=1111)
 #Initialize the class
-Hest = base_heston(mu=mu, rho=rho, sigma=sigma, theta=theta, kappa=kappa, K=K)
+Hest = base_heston(mu=mu, rho=rho, sigma=sigma, theta=theta, kappa=kappa, K=K, nu = nu)
 
 #Monte Carlo simulation - create the different paths
 for i in range(0,num_sims):
